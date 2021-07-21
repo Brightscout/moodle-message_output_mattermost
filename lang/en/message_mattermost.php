b7f6af2af15d23a1a75a25766c1a5835fe15baee
Hrishav Kumar <33994932+hrishavkmr@users.noreply.github.com>
2021-07-21 15:30:45 +0530

    add initial setup code (#1)
    * add initial code
    
    * MI-1336 update comments

---
+++ lang/en/message_mattermost.php
@@ -0,0 +1,30 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package   message_mattermost
+ * @copyright 2020, Hrishav Kumar <hrishav.kumar@brightscout.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+*/
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['notconfigured'] = 'The Mattermost server hasn\'t been configured so Mattermost messages cannot be sent';
+$string['pluginname'] = 'Mattermost';
+$string['secret'] = 'Webhook secret';
+$string['serverurl'] = 'Mattermost Server URL';
+$string['configserverurl'] = 'Mattermost server URL where notification will be sent.';
+$string['configsecret'] = 'Mattermost notification plugin secret which is installed on the server above.';
---
+++ message_output_mattermost.php
@@ -0,0 +1,122 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package   message_mattermost
+ * @copyright 2020, Hrishav Kumar <hrishav.kumar@brightscout.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+*/
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot.'/message/output/lib.php');
+
+class message_output_mattermost extends message_output {
+
+    /**
+     * Processes the message and sends a notification via mattermost
+     *
+     * @param stdClass $eventdata the event data submitted by the message sender plus $eventdata->savedmessageid
+     * @return true if ok, false if error
+     */
+    public function send_message($eventdata) {
+        global $CFG;
+
+        // Skip any messaging of suspended and deleted users.
+        if (($eventdata->userto->auth === 'nologin') || $eventdata->userto->suspended || $eventdata->userto->deleted) {
+            return true;
+        }
+
+        if (!empty($CFG->noemailever)) {
+            // Hidden setting for development sites, set in config.php if needed.
+            debugging('$CFG->noemailever is active, no matttermost message sent.', DEBUG_MINIMAL);
+            return true;
+        }
+
+        // TODO: Add mattermost manager send message call here.
+        // return $this->matttermostmanager->send_message($eventdata->fullmessage, $eventdata->userto->id);
+        return true;
+    }
+
+    /**
+     * Creates necessary fields in the messaging config form.
+     *
+     * @param array $preferences An object of user preferences
+     */
+    public function config_form($preferences) {
+        global $USER;
+        if (!$this->is_system_configured()) {
+            return get_string('notconfigured', 'message_matttermost');
+        } else {
+            // TODO: Add mattermost manager config form call here.
+            return true;
+            // return $this->matttermostmanager->config_form($preferences, $USER->id);
+        }
+    }
+
+    /**
+     * Parses the submitted form data and saves it into preferences array.
+     *
+     * @param stdClass $form preferences form class
+     * @param array $preferences preferences array
+     */
+    public function process_form($form, &$preferences) {
+        // This is handled by the callback in matttermostconnect.php. If there is ever a form to do this, it would need similar options
+        // as in "manager::function set_user_connection".
+        return true;
+    }
+
+    /**
+     * Loads the config data from database to put on the form during initial form display.
+     * There is no real form for this, so these are here primarily for documentation purposes.
+     *
+     * @param object $preferences preferences object
+     * @param int $userid the user id
+     */
+    public function load_data(&$preferences, $userid) {
+        // $preferences->matttermost_user_id = get_user_preferences('message_processor_matttermost_user_id', '', $userid);
+        // $preferences->matttermost_access_token = get_user_preferences('message_processor_matttermost_access_token', '', $userid);
+        // $preferences->matttermost_channel = get_user_preferences('message_processor_matttermost_channel', '', $userid);
+        // $preferences->matttermost_channel_id = get_user_preferences('message_processor_matttermost_channel_id', '', $userid);
+        // $preferences->matttermost_configuration_url = get_user_preferences('message_processor_matttermost_configuration_url', '', $userid);
+        // $preferences->matttermost_url = get_user_preferences('message_processor_matttermost_url', '', $userid);
+    }
+
+    /**
+     * Tests whether the matttermost settings have been configured
+     * @return boolean true if matttermost is configured
+     */
+    public function is_system_configured() {
+        return true;
+        // return (!empty($this->matttermostmanager->config('clientid')) && !empty($this->matttermostmanager->config('clientsecret')));
+    }
+
+    /**
+     * Tests whether the matttermost settings have been configured on user level
+     * @param  object $user the user object, defaults to $USER.
+     * @return bool has the user made all the necessary settings
+     * in their profile to allow this plugin to be used.
+     */
+    public function is_user_configured($user = null) {
+        global $USER;
+
+        if ($user === null) {
+            $user = $USER;
+        }
+        return true;
+        // return $this->matttermostmanager->validate_user_connection($user->id);
+    }
+}
